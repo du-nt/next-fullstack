8662d0361e670046a14965150e3dea577bbf4996
dunt <ndu@infinitalk.co.jp>
2023-11-27 10:06:19 +0900

    feat: install prisma

--- libs/authOptions.ts
+++ libs/authOptions.ts
@@ -1,11 +1,18 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+import prisma from './db'
+
 const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt'
+  },
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
     })
   ],
   secret: process.env.NEXTAUTH_SECRET,
